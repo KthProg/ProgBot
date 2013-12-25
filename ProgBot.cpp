8840a89f52b53a27dea12041e8f355562293f64a
KthProg <kthprog@gmail.com>
2013-12-25 01:08:36 -0500

    Update ProgBot.cpp

--- ProgBot.cpp
+++ ProgBot.cpp
@@ -1,23 +1,49 @@
-#include "TCPClient.h"
+#include "ProgBot.h"
 
-int main()
+void ProgBot::doCmd()
 {
-	TCPClient tcpc("6667", "irc.hackthissite.org");
-	tcpc.readSocket();
-	tcpc.writeSocket(tcpc.login);
-	tcpc.readSocket();
-	string recieved = "";
+	parseInput(readSocket());
 
-	while(true)
+	if(input["cmd"] == "pong")
 	{
-		try
-		{
-			recieved = tcpc.readSocket();
-		}
-		catch(exception e)
-		{
-			cout << "Uncaught exception: " << e.what() << endl;
-		}
+		pong();
 	}
-	return 0;
+	else
+	if(input["cmd"] == "say")
+	{
+		say();
+	}
 }
+
+void ProgBot::parseInput(string cmd)
+{
+	try
+	{
+		cmd = cmd.substr(1, string::npos);
+		input["nick"] = cmd.substr(0, cmd.find('!',0));
+		cmd = cmd.substr(cmd.find('!',0) + 1,string::npos);
+		input["room"] = cmd.substr(cmd.find('#',0), cmd.find(':',0) - cmd.find('#',0));
+		cmd = cmd.substr(cmd.find(':',0) + 1,string::npos);
+		input["cmd"] = cmd.substr(cmd.find('!',0) + 2, cmd.find(' ',cmd.find('!',0) + 2) - (cmd.find('!',0) + 2));
+		cmd = cmd.substr(cmd.find(' ',cmd.find('!',0) + 2) + 1, string::npos);
+		input["input"] = cmd;
+	}
+	catch(exception e)
+	{
+		cout << "Exception while parsing user input: " << e.what() << endl;
+	}
+}
+
+void ProgBot::pong()
+{
+	writeSocket("PONG :" + input["input"] + "\n");
+	writeSocket("JOIN #coffeesh0p\n");
+}
+
+void ProgBot::say()
+{
+	writeSocket("PRIVMSG " + input["room"] + ":" + input["input"] + "\n");
+}
+
+string ProgBot::login = "NICK ProgBot\nUSER  ProgBot 8 *  : Prog Bot\n";
+string ProgBot::prefix = "!P";
