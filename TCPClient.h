2cd6d2b96eff2c820f6946ae7598e55206c8445b
KthProg <kthprog@gmail.com>
2013-12-25 01:07:18 -0500

    Update TCPClient.h

--- TCPClient.h
+++ TCPClient.h
@@ -1,49 +1,33 @@
 #ifndef _TCPCLIENT_DEF
+#define _TCPCLIENT_DEF
 
 #include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <iostream>
-//#include <map>
+#include <map>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
-struct userInput
-{
-	string nick;
-	string room;
-	struct userText
-	{
-		string cmd;
-		string input;
-	} uTxt;
-};
+
 
 class TCPClient
 {
-private:
+protected:
 	SOCKET TCPSock;
-	userInput userIn;
-	//map<string, void (TCPClient::*)()> fm;
+	map <string, string> input;
 public:
-	static string login;
-	static string prefix;
-	TCPClient();
 	TCPClient(string port, string host);
 	~TCPClient();
 	string readSocket();
 	void writeSocket(string write);
 	void TCPConnect(string port, string host);
 	void TCPDisconnect();
-	void doCmd();
-	void getInput(string cmd);
-	//void makeMap();
-	void pong();
-	void say();
+	virtual void doCmd();
+	virtual void parseInput(string cmd);
 };
 
-#define _TCPCLIENT_DEF
 #endif
