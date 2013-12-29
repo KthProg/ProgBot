258e63e0eb61c6ca3ec95ba48521f1d452b8b939
KthProg <kthprog@gmail.com>
2013-12-29 09:48:36 -0500

    Update Start.cpp

--- Start.cpp
+++ Start.cpp
@@ -4,8 +4,10 @@ int main()
 {
 	ProgBot prog("6667", "irc.hackthissite.org");
 	prog.readSocket();
+	cout << prog;
 	prog.writeSocket(prog.login);
 	prog.readSocket();
+	cout << prog;
 
 	while(true)
 	{
