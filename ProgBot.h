8a80323cfb438caf9bc311b88406f58ddf395773
KthProg <kthprog@gmail.com>
2013-12-29 09:47:56 -0500

    Update ProgBot.h

--- ProgBot.h
+++ ProgBot.h
@@ -1,18 +1,40 @@
 #ifndef _PROGBOT_DEF
 #define _PROGBOT_DEF
 
-#include "TCPClient.h"
+#include "IRCBot.h"
+#include <array>
+#include <random>
+#include <time.h>
 
-class ProgBot : public TCPClient
+class ProgBot : public IRCBot
 {
 public:
-	ProgBot(string port, string host):TCPClient(port, host){};
-	static string login;
-	static string prefix;
+	ProgBot(string port, string host):IRCBot(port, host)
+	{
+		board.fill('e'); 
+		gInt = 0;
+		prefix = "P! ";
+		login = "NICK ProgBot\nUSER  ProgBot 8 *  : Prog Bot\r\n";
+
+		split.push_back(":");	split.push_back("!");
+		split.push_back("#");	split.push_back(":");
+		split.push_back(prefix);split.push_back(" ");
+		split.push_back("");	split.push_back("\n");
+
+		items.push_back("user");
+		items.push_back("room");
+		items.push_back("cmd");
+		items.push_back("input");
+	}
+
 	void doCmd();
-	void parseInput(string cmd);
-	void pong();
+	//void parseInput();
+
 	void say();
+	void ttt(); //tic tac toe
+	array<char, 9> board;
+	void gNum(); //guess a number
+	int gInt;
 };
 
 #endif
